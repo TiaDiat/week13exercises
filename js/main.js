2c47880e8314117d3e3408d944232e48563e4b80
Ekaterina Diatchenko <t1ad1at@gmail.com>
2018-08-06 17:05:59 -0700

    complete

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var myStr = ["Hello", "My", "Friends", "toDay", "THiS"]
+var myStr = ["Hello", "My", "Friends", "toDay", "THiS"];
 
 //change each string to lowercase, and then add them to a new array
 var newArray = [];
 
 for (i=0; i<myStr.length; i++) {
 		newArray.push(myStr[i].toLowerCase());
-		}
+		};
 
 //loop through the new array (all lowercase strings) and log the first and last character of each string to the console
 
@@ -22,9 +22,21 @@ var splitName = name.split("");
 for (i=0; i<splitName.length; i++) {
 
 		console.log(splitName[i]);
-	}
+	};
 
 //using the join method, put your name back together and replace all occurrences of one letter with another
 	splitName = splitName.join(" ");
 	console.log(splitName.replace(/a/g, "z"));
+
+//create a function that will get the user's input, test it against the standard regular expression for emails, and log the result in the console
+
+function validateEmail() {
+  var userEmail = document.getElementById("userEmail");
+  var regEx = /^[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,5}$/;
+
+  console.log(regEx.test(userEmail.value));
+ };
+
+//make the function "listen" to the button using addEventListener()
+document.getElementById("testEmail").addEventListener("click", validateEmail);
 	
